8e35cfc54c4aeceacd3fc9ecb710da30c1389d84
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-09-13 23:41:27 -0300

    idk

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-from menu import Menu
+import time
+import tkinter as tk
 
-if __name__ == "__main__":
-    app = Menu()
-    app.mainloop()
+root = tk.Tk()
+
+root.title("Inventario")
+root.geometry("800x600")
+root.resizable(False, False)
+
+#Intentar meterlo en clases
+menu = tk.Frame(root)
+menu.configure(width=800, height=600, bg="blue")
+
+
+titulo = tk.Label(menu, text="INVENTARIO")
+titulo.pack()
+
+textoHora = tk.Label(menu)
+textoHora.pack()
+
+def actualizarHora():
+    textoHora.config(text=time.strftime("%H:%M:%S"))
+    root.after(1000, actualizarHora)
+
+
+actualizarHora()
+menu.pack()
+root.mainloop()
